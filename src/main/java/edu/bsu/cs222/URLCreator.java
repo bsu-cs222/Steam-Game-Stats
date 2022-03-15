7b4178b007e7200f04c5400ebc350684147c1735
caige <caigerogers12@gmail.com>
2022-03-15 17:24:10 -0400

    Made first failed Test Class for URLCreator

---
+++ src/main/java/edu/bsu/cs222/URLCreator.java
@@ -0,0 +1,8 @@
+package edu.bsu.cs222;
+
+public class URLCreator {
+
+    public String urlMaker(String input) {
+        return "";
+    }
+}
---
+++ src/test/java/edu/bsu/cs222/URLCreatorTest.java
@@ -0,0 +1,13 @@
+package edu.bsu.cs222;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class URLCreatorTest {
+    @Test
+    public void url(){
+        URLCreator urlCreator = new URLCreator();
+        String result = urlCreator.urlMaker("Forza");
+        Assertions.assertEquals("Forza", result);
+    }
+}
