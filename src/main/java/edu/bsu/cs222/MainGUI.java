b71617545001d824a8ffe2fdcd4d01d8926ff0d3
Kyle Conn <kyle1335conn@gmail.com>
2022-03-20 23:29:07 -0400

    line graph makes a sample graph of Fallout 4, removed unused methods and imports.

--- src/main/java/edu/bsu/cs222/MainGUI.java
+++ src/main/java/edu/bsu/cs222/MainGUI.java
@@ -9,16 +9,6 @@ import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -51,88 +41,66 @@ public class MainGUI extends Application {
         searchTextField.textProperty().addListener((ov, t, t1) -> searchButton.setDisable(searchTextField.getText().equals("")));
     }
 
-    private void makeRedirectButton(ArrayList<String> gameNameArrayList) {
-        if (gameNameArrayList.size() == 1) {
-            runSearch();
-        } else {
-            Label redirectLabel = new Label("Did you mean:");
-            searchVbox.getChildren().add(redirectLabel);
-            for (String gameName : gameNameArrayList) {
-                Button button = new Button(gameName);
-                searchVbox.getChildren().add(button);
-                button.setOnAction((event) -> runSearch());
-            }
-        }
-    }
-
     private void runSearch() {
         searchButton.setDisable(true);
         searchTextField.setDisable(true);
-        executor.execute(() -> {
-            Platform.runLater(this::updateGamePrice);
-        });
-    }
-
-    public static void main(String[] args) {
-        MainGUI steamSearch = new MainGUI();
-        Scanner scanner = new Scanner(System.in);
-        String gameName = scanner.nextLine();
-        try {
-            String plains = steamSearch.getLatestRevisionOf(gameName);
-            System.out.println();
-        } catch (IOException ioException) {
-            System.err.println("Network connection problem: " + ioException.getMessage());
-        }
-    }
-
-    private String getLatestRevisionOf(String gameName) throws IOException {
-        String urlString = String.format("https://isthereanydeal.com/#/page:game/info?plain=monsterhunterrise", gameName);
-        String encodedUrlString = URLEncoder.encode(urlString, Charset.defaultCharset());
-        try {
-            URL url = new URL(encodedUrlString);
-            URLConnection connection = url.openConnection();
-            connection.setRequestProperty("User-Agent", "Steam-Game-Stats/0.1");
-            InputStream inputStream = connection.getInputStream();
-            SteamSearch parser = new SteamSearch();
-            String plains = parser.parse(inputStream);
-            return plains;
-        } catch (MalformedURLException malformedURLException) {
-            throw new RuntimeException(malformedURLException);
-        }
+        executor.execute(() -> Platform.runLater(this::updateGamePrice));
+        searchButton.setDisable(false);
+        searchTextField.setDisable(false);
     }
 
     private void updateGamePrice() {
         makeGamePriceChart();
-        searchButton.setDisable(false);
-        searchTextField.setDisable(false);
+        searchLabel.setText("Price tab has been updated");
     }
 
     private void makeGamePriceChart() {
         final NumberAxis xAxis = new NumberAxis();
         final NumberAxis yAxis = new NumberAxis();
-        xAxis.setLabel("Number of Month");
+        xAxis.setLabel("Month");
         yAxis.setLabel("Price ($)");
-        final LineChart<Number,Number> priceHistoryLineChart = new LineChart<Number,Number>(xAxis,yAxis);
+        final LineChart<Number,Number> priceHistoryLineChart = new LineChart<>(xAxis, yAxis);
 
         priceHistoryLineChart.setTitle("Price History");
         XYChart.Series steamSeries = new XYChart.Series();
         steamSeries.setName("Steam");
-        steamSeries.getData().add(new XYChart.Data(1, 23));
-        steamSeries.getData().add(new XYChart.Data(2, 14));
-        steamSeries.getData().add(new XYChart.Data(2, 15));
-        steamSeries.getData().add(new XYChart.Data(4, 24));
-        steamSeries.getData().add(new XYChart.Data(4, 34));
-        steamSeries.getData().add(new XYChart.Data(6, 36));
-        steamSeries.getData().add(new XYChart.Data(6, 22));
-        steamSeries.getData().add(new XYChart.Data(8, 45));
-        steamSeries.getData().add(new XYChart.Data(8, 43));
-        steamSeries.getData().add(new XYChart.Data(10, 17));
-        steamSeries.getData().add(new XYChart.Data(10, 29));
-        steamSeries.getData().add(new XYChart.Data(12, 25));
-
-        /*for (int numberOfPriceChanges = 0; numberOfPriceChanges <= priceHistoryArrayList.size(); numberOfPriceChanges++) {
-            steamSeries.getData().add(new XYChart.Data(numberOfPriceChanges, priceHistoryArrayList.get(numberOfPriceChanges)));
-        }*/
+        //sample graph based on Fallout 4 TODO make a for loop to cycle through game price changes and make data points
+        steamSeries.getData().add(new XYChart.Data(-12, 29.99));
+        steamSeries.getData().add(new XYChart.Data(-11.75, 29.99));
+        steamSeries.getData().add(new XYChart.Data(-11.75, 14.99));
+        steamSeries.getData().add(new XYChart.Data(-11.5, 14.99));
+        steamSeries.getData().add(new XYChart.Data(-11.5, 29.99));
+        steamSeries.getData().add(new XYChart.Data(-10.5, 29.99));
+        steamSeries.getData().add(new XYChart.Data(-10.5, 14.99));
+        steamSeries.getData().add(new XYChart.Data(-10, 14.99));
+        steamSeries.getData().add(new XYChart.Data(-10, 29.99));
+        steamSeries.getData().add(new XYChart.Data(-9.5, 29.99));
+        steamSeries.getData().add(new XYChart.Data(-9.5, 11.99));
+        steamSeries.getData().add(new XYChart.Data(-8, 11.99));
+        steamSeries.getData().add(new XYChart.Data(-8, 29.99));
+        steamSeries.getData().add(new XYChart.Data(-7.9, 29.99));
+        steamSeries.getData().add(new XYChart.Data(-7.9, 19.99));
+        steamSeries.getData().add(new XYChart.Data(-7, 19.99));
+        steamSeries.getData().add(new XYChart.Data(-7, 7.99));
+        steamSeries.getData().add(new XYChart.Data(-6, 7.99));
+        steamSeries.getData().add(new XYChart.Data(-6, 19.99));
+        steamSeries.getData().add(new XYChart.Data(-4.75, 19.99));
+        steamSeries.getData().add(new XYChart.Data(-4.75, 4.99));
+        steamSeries.getData().add(new XYChart.Data(-4, 4.99));
+        steamSeries.getData().add(new XYChart.Data(-4, 19.99));
+        steamSeries.getData().add(new XYChart.Data(-3, 19.99));
+        steamSeries.getData().add(new XYChart.Data(-3, 7.99));
+        steamSeries.getData().add(new XYChart.Data(-2.75, 7.99));
+        steamSeries.getData().add(new XYChart.Data(-2.75, 19.99));
+        steamSeries.getData().add(new XYChart.Data(-2, 19.99));
+        steamSeries.getData().add(new XYChart.Data(-2, 7.99));
+        steamSeries.getData().add(new XYChart.Data(-1.5, 7.99));
+        steamSeries.getData().add(new XYChart.Data(-1.5, 19.99));
+        steamSeries.getData().add(new XYChart.Data(-1, 19.99));
+        steamSeries.getData().add(new XYChart.Data(-1, 7.99));
+        steamSeries.getData().add(new XYChart.Data(-.75, 7.99));
+        steamSeries.getData().add(new XYChart.Data(-.75, 19.99));
+        steamSeries.getData().add(new XYChart.Data(0, 19.99));
 
         priceHistoryLineChart.getData().add(steamSeries);
         priceTab.setContent(priceHistoryLineChart);
