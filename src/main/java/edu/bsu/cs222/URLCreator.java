474cb5186aef1c2c3de24184d8431352e1eb9312
caige <caigerogers12@gmail.com>
2022-03-24 19:39:12 -0400

    Added jsonToString to call API, made a separate method to format url, added dependencies

--- src/main/java/edu/bsu/cs222/URLCreator.java
+++ src/main/java/edu/bsu/cs222/URLCreator.java
@@ -1,8 +1,39 @@
 package edu.bsu.cs222;
 
-public class URLCreator {
+import com.jayway.jsonpath.JsonPath;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import net.minidev.json.JSONArray;
+import net.minidev.json.writer.JsonReader;
+import org.netbeans.html.json.impl.JSON;
 
-    public String urlMaker(String input) {
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class URLCreator {
+    public String urlMaker(String input){
+        Client client = ClientBuilder.newClient();
+        Response response = client.target(urlDesigner(input))
+                .request(MediaType.TEXT_PLAIN_TYPE)
+                .get();
+        // called api to give json data
+
+        //insert below for method to call current price
+        jsonToString(response.readEntity(JSON.class));
+
+
         return "";
+    }
+
+    private String jsonToString(JSON json) {
+        JSONArray jsonRedirects = JsonPath.read(json,"$..Price");
+        String body = jsonRedirects.get(0).toString();
+        return body;
+    }
+
+    public String urlDesigner(String input){
+        return String.format("https://api.isthereanydeal.com/v01/game/prices/?key=420d3d4cd304e25e8b0ac4e1a58dfa406283946d&plains=%s&region=us&country=US&shops=steam",input);
     }
 }
--- src/test/java/edu/bsu/cs222/URLCreatorTest.java
+++ src/test/java/edu/bsu/cs222/URLCreatorTest.java
@@ -3,9 +3,11 @@ package edu.bsu.cs222;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.net.MalformedURLException;
+
 public class URLCreatorTest {
     @Test
-    public void urlTest(){
+    public void urlTest() throws MalformedURLException {
         URLCreator urlCreator = new URLCreator();
         String result = urlCreator.urlMaker("Forza");
         Assertions.assertEquals("Forza", result);
