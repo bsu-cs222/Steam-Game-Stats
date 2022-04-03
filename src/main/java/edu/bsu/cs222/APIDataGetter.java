9018689136e47ab977b73c45aa42d0d26220192c
caige <caigerogers12@gmail.com>
2022-04-03 16:39:26 -0400

    removed useless calls

--- src/main/java/edu/bsu/cs222/APIDataGetter.java
+++ src/main/java/edu/bsu/cs222/APIDataGetter.java
@@ -10,9 +10,6 @@ import net.minidev.json.JSONArray;
 import static edu.bsu.cs222.URLCreator.urlDesigner;
 
 public class APIDataGetter {
-    public static void main(String[] args) {
-        historicalLowData("falloutiv");
-    }
     public static String currentPriceData(String input){
         Client client = ClientBuilder.newClient();
         Response response = client.target(urlDesigner(input))
@@ -35,8 +32,6 @@ public class APIDataGetter {
         double priceAsDouble = Double.parseDouble(jsonPrice);
         String priceCeil = String.valueOf(Math.ceil(priceAsDouble));
         priceCeil = priceCeil.replace(".0","");
-        String title = "Forzaii";
-        System.out.println(title.compareTo("forzaiii"));
         return priceCeil;
     }
 
