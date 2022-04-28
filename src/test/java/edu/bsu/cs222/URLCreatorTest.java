ff09237aa00f34f6e97ac2e9214c4bc02c251e00
caige <caigerogers12@gmail.com>
2022-04-28 19:41:23 -0400

    fixed spacing and put an private access modifier in URLCreatorTest

--- src/test/java/edu/bsu/cs222/URLCreatorTest.java
+++ src/test/java/edu/bsu/cs222/URLCreatorTest.java
@@ -4,11 +4,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class URLCreatorTest {
-    KeyHolder keyHolder = new KeyHolder();
+    private final KeyHolder keyHolder = new KeyHolder();
+    private final URLCreator urlCreator =  new URLCreator();
 
     @Test
     public void currentPriceDataTest() {
-        URLCreator urlCreator =  new URLCreator();
         String expected = String.format("https://api.isthereanydeal.com/v01/game/overview/?key=%s&region=us&country=US&plains=falloutiv&shop=steam&allowed=steam", keyHolder.keyReturner());
         String actual = urlCreator.urlSearch("falloutiv", "steam");
         Assertions.assertEquals(expected, actual);
